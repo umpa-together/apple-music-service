8c5f9f5c067894e6a97892861db994fe586bd250
blackSooooo <blacksooooo@naver.com>
2023-01-06 01:05:36 +0900

    initial commit

---
+++ src/main/kotlin/com/umpa/applemusic/AppleMusicApplication.kt
@@ -0,0 +1,11 @@
+package com.umpa.applemusic
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AppleMusicApplication
+
+fun main(args: Array<String>) {
+	runApplication<AppleMusicApplication>(*args)
+}
---
+++ src/test/kotlin/com/umpa/applemusic/AppleMusicApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.umpa.applemusic
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AppleMusicApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
