ee8adb3269d7124d27404c7f6067f4f66aaeafc5
blackSooooo <blacksooooo@naver.com>
2023-01-07 15:19:11 +0900

    feat: 서비스 레이어 구현

--- src/main/kotlin/com/umpa/applemusic/domain/AppleMusicService.kt
+++ src/main/kotlin/com/umpa/applemusic/domain/AppleMusicService.kt
@@ -1,29 +1,33 @@
 package com.umpa.applemusic.domain
 
 import com.umpa.applemusic.clients.AppleMusicClient
+import com.umpa.applemusic.clients.AppleMusicHintResponse
+import com.umpa.applemusic.clients.AppleMusicResultResponse
+import com.umpa.applemusic.support.enums.AppleMusicType
 import org.springframework.stereotype.Service
 
 @Service
 class AppleMusicService(
     private val appleMusicClient: AppleMusicClient
 ) {
-    fun searchSongsByKeyword(keyword: String) {
-        appleMusicClient.getResultsByKeyword(keyword)
+    fun searchSongsByKeyword(keyword: String): AppleMusicResultResponse {
+        // TODO keyword -> RecentKeyword 등록
+        return appleMusicClient.getResultsByKeywordAndType(keyword, AppleMusicType.SONGS)
     }
 
-    fun searchArtistsByKeyword(keyword: String) {
-        // TODO
+    fun searchArtistsByKeyword(keyword: String): AppleMusicResultResponse {
+        return appleMusicClient.getResultsByKeywordAndType(keyword, AppleMusicType.ARTISTS)
     }
 
-    fun searchAlbumsByKeyword(keyword: String) {
-        // TODO
+    fun searchAlbumsByKeyword(keyword: String): AppleMusicResultResponse {
+        return appleMusicClient.getResultsByKeywordAndType(keyword, AppleMusicType.ALBUMS)
     }
 
-    fun searchNextByCursorKey(cursorKey: String) {
-        // TODO
+    fun searchNextByCursorKey(cursorKey: String): AppleMusicResultResponse {
+        return appleMusicClient.getNextByCursorKey(cursorKey)
     }
 
-    fun searchHintsByKeyword(keyword: String) {
-        // TODO
+    fun searchHintsByKeyword(keyword: String): AppleMusicHintResponse {
+        return appleMusicClient.getHintsByKeyword(keyword)
     }
 }
