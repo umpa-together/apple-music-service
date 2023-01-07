080f300e8f7e58f512becb5fcd8be0808af72366
blackSooooo <blacksooooo@naver.com>
2023-01-07 16:51:28 +0900

    feat: global error handling을 위한 controller advice 추가

---
+++ src/main/kotlin/com/umpa/applemusic/controller/ApiControllerAdvice.kt
@@ -0,0 +1,38 @@
+package com.umpa.applemusic.controller
+
+import com.umpa.applemusic.support.error.ApplicationException
+import com.umpa.applemusic.support.error.DefaultErrorTypeType
+import com.umpa.applemusic.support.error.ErrorResponse
+import com.umpa.applemusic.support.error.FeignClientException
+import org.slf4j.LoggerFactory
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class ApiControllerAdvice {
+    private val logger = LoggerFactory.getLogger(ApiControllerAdvice::class.java)
+
+    @ExceptionHandler(ApplicationException::class)
+    fun handleApplicationException(e: ApplicationException): ResponseEntity<ErrorResponse> {
+        logger.info("ApplicationException: {}", e.message, e)
+        return ResponseEntity.status(e.errorType.statusCode.value())
+            .body(ErrorResponse(e.errorType.message, e.errorType.statusCode.value()))
+    }
+
+    @ExceptionHandler(FeignClientException::class)
+    fun handleFeignException(e: FeignClientException): ResponseEntity<ErrorResponse> {
+        logger.info("FeignException: {}", e.message, e)
+        val errorType = DefaultErrorTypeType.INTERNAL_SERVER_ERROR
+        return ResponseEntity.status(errorType.statusCode.value())
+            .body(ErrorResponse(errorType.message, errorType.statusCode.value()))
+    }
+
+    @ExceptionHandler(RuntimeException::class)
+    fun handleRuntimeException(e: RuntimeException): ResponseEntity<ErrorResponse> {
+        logger.info("RuntimeException: {}", e.message, e)
+        val errorType = DefaultErrorTypeType.INTERNAL_SERVER_ERROR
+        return ResponseEntity.status(errorType.statusCode.value())
+            .body(ErrorResponse(errorType.message, errorType.statusCode.value()))
+    }
+}
