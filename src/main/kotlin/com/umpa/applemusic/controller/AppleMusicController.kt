904d313f7b1fe1ca89a8ec49c723520f89a4978f
blackSooooo <blacksooooo@naver.com>
2023-01-07 15:19:19 +0900

    feat: 컨트롤러 레이어 구현

--- src/main/kotlin/com/umpa/applemusic/controller/AppleMusicController.kt
+++ src/main/kotlin/com/umpa/applemusic/controller/AppleMusicController.kt
@@ -1,6 +1,9 @@
 package com.umpa.applemusic.controller
 
+import com.umpa.applemusic.clients.AppleMusicHintResponse
+import com.umpa.applemusic.clients.AppleMusicResultResponse
 import com.umpa.applemusic.domain.AppleMusicService
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,39 +17,40 @@ class AppleMusicController(
 ) {
 
     @GetMapping(
-        value = ["/songs/{keyword}"]
+        value = ["/songs/{keyword:.+}"]
     )
     fun searchSongsByKeyword(
         @PathVariable keyword: String
-    ) {
-        // TODO
+    ): ResponseEntity<AppleMusicResultResponse> {
+        return ResponseEntity.ok(appleMusicService.searchSongsByKeyword(keyword))
     }
 
     @GetMapping(
-        value = ["/artists/{keyword}"]
+        value = ["/artists/{keyword:.+}"]
     )
     fun searchArtistsByKeyword(
         @PathVariable keyword: String
-    ) {
-        // TODO
+    ): ResponseEntity<AppleMusicResultResponse> {
+        return ResponseEntity.ok(appleMusicService.searchArtistsByKeyword(keyword))
     }
 
     @GetMapping(
-        value = ["/albums/{keyword}"]
+        value = ["/albums/{keyword:.+}"]
     )
     fun searchAlbumsByKeyword(
         @PathVariable keyword: String
-    ) {
-        // TODO
+    ): ResponseEntity<AppleMusicResultResponse> {
+        return ResponseEntity.ok(appleMusicService.searchAlbumsByKeyword(keyword))
     }
 
+    // cursorKey의 경우, 특수문자가 포함돼서 넘어오기 때문에 클라에서 urlEncoding 해서 넘겨야 됨.
     @GetMapping(
         value = ["/next"]
     )
     fun searchNextByCursorKey(
         @RequestParam cursorKey: String
-    ) {
-        // TODO
+    ): ResponseEntity<AppleMusicResultResponse> {
+        return ResponseEntity.ok(appleMusicService.searchNextByCursorKey(cursorKey))
     }
 
     @GetMapping(
@@ -54,7 +58,7 @@ class AppleMusicController(
     )
     fun searchHintsByKeyword(
         @RequestParam keyword: String
-    ) {
-        // TODO
+    ): ResponseEntity<AppleMusicHintResponse> {
+        return ResponseEntity.ok(appleMusicService.searchHintsByKeyword(keyword))
     }
 }
