4607f38dec5ad0634bcf0f31954646e786ebb095
blackSooooo <blacksooooo@naver.com>
2023-01-07 15:19:40 +0900

    feat: appleMusic feign 구현

--- src/main/kotlin/com/umpa/applemusic/clients/AppleMusicApi.kt
+++ src/main/kotlin/com/umpa/applemusic/clients/AppleMusicApi.kt
@@ -2,8 +2,8 @@ package com.umpa.applemusic.clients
 
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestParam
 
 @FeignClient(
@@ -18,16 +18,9 @@ interface AppleMusicApi {
     fun getResultsByName(
         @RequestParam term: String,
         @RequestParam limit: Int = LIMIT,
-    )
-
-    @GetMapping(
-        value = ["/v1/catalog/kr/search"],
-        consumes = [MediaType.APPLICATION_JSON_VALUE]
-    )
-    fun getNextByCursorKey(
-        cursorKey: String,
-        @RequestParam limit: Int = LIMIT
-    )
+        @RequestParam types: String,
+        @RequestParam offset: Int = 0
+    ): ResponseEntity<LinkedHashMap<String, LinkedHashMap<String ,Any>>>
 
     @GetMapping(
         value = ["/v1/catalog/kr/search/hints"],
@@ -36,7 +29,7 @@ interface AppleMusicApi {
     fun getHintsByKeyword(
         @RequestParam term: String,
         @RequestParam limit: Int = HINT_LIMIT
-    )
+    ): ResponseEntity<LinkedHashMap<String, LinkedHashMap<String, Any>>>
 
     companion object {
         const val LIMIT = 20
