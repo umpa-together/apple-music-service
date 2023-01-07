9ed2c664b0a2ba9c2de84f843f1a2354ff0264d5
blackSooooo <blacksooooo@naver.com>
2023-01-07 15:17:20 +0900

    feat: jjwt 이용을 위한 추가적인 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
 	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	implementation("io.jsonwebtoken:jjwt-gson:0.11.5")
 
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
